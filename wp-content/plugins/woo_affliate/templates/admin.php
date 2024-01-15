5d446f019b0503b2b4ffb7624407b39a331970c2
Ton Nguyen Hoang An <an.tonnguyenhoang@gmail.com>
2024-01-15 23:08:20 +0700

    Commit update plugin

--- wp-content/plugins/woo_affliate/templates/admin.php
+++ wp-content/plugins/woo_affliate/templates/admin.php
@@ -3,10 +3,17 @@
   $successMessage = '';
   $errorMessage = '';
   $tableUser = $wpdb->prefix . 'users';
+  $tableUserCommission = $wpdb->prefix . 'woo_history_user_commission';
+  $status = [
+    'PURCHASE' => 1,
+    'USE_POINT' => 2,
+    'USE_POINT_IN_PROCESS' => 4,
+  ];
 
   $users = $wpdb->get_results( 'SELECT * FROM ' . $tableUser . ' ORDER BY id ASC', ARRAY_A );
+  $userCommissions = $wpdb->get_results( 'SELECT * FROM ' . $tableUserCommission . ' ORDER BY id ASC', ARRAY_A );
 
-  $currentPage = (! empty( $_GET['paged'] )) && ($_GET['tab'] == 'dstv') ? (int) $_GET['paged'] : 1;
+  $currentPage = (! empty( $_GET['paged'] )) && ($_GET['tab'] == 'setting1') ? (int) $_GET['paged'] : 1;
   $total = count( $users );
   $perPage = 10;
   $totalPages = ceil($total/ $perPage);
@@ -48,7 +55,7 @@
           </tr>
         </thead>
         <tbody>
-          <?php foreach ( $users as $key => $user ) { ?>
+          <?php foreach ( $users as $user ) { ?>
             <tr>
               <td><?php echo $user['display_name'] ?></td>
               <td>
@@ -88,7 +95,9 @@
     </div>
   </div>
   <div class="overlay d-none"></div>
-  <?php foreach ( $users as $key => $user ) { ?>
+  <?php foreach ( $users as $key => $user ) { 
+    $childUser = $wpdb->get_results( 'SELECT * FROM ' . $tableUser . ' WHERE user_parent = ' . $user['ID'] . ' ORDER BY id ASC', ARRAY_A );
+  ?>
     <div class="modal d-none modal-lower-level-<?php echo $user['ID']; ?>">
       <div class="modal-wrapper">
         <p onclick="closeLowerModal(<?php echo $user['ID']; ?>)" class="close">✕</p>
@@ -99,34 +108,79 @@
           <table class="wp-list-table widefat fixed striped table-view-list users">
             <thead>
               <tr>
-                <th>Tên</th>
-                <th>Cấp</th>
-                <th>Doanh thu</th>
+                <th>Tên cấp dưới</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>User 1</td>
-                <td>Cấp 1</td>
-                <td>1.000.000</td>
-              </tr>
-              <tr>
-                <td>User 2</td>
-                <td>Cấp 1</td>
-                <td>1.000.000</td>
-              </tr>
-              <tr>
-                <td>User 3</td>
-                <td>Cấp 2</td>
-                <td>1.000.000</td>
-              </tr>
+              <?php if (count($childUser) === 0) { ?>
+                <tr>
+                  <td>Không có cấp dưới</td>
+                </tr>
+              <?php } else {
+                foreach ($childUser as $child) { 
+              ?>
+                <tr>
+                  <td><?php echo $child['display_name']; ?></td>
+                </tr>
+            <?php } }?>
             </tbody>
           </table>
         </div>
       </div>
     </div>
   <?php } ?>
-  <?php foreach ( $users as $key => $user ) { ?>
+  <?php foreach ( $users as $user ) { 
+    $waitingReview = $wpdb->get_results('SELECT SUM(commission) as waitingReview FROM ' . $tableUserCommission . ' WHERE user_id = ' . $user['ID'] . ' AND status = ' . $status['USE_POINT_IN_PROCESS']);
+    $actuallyReceive = $wpdb->get_results('SELECT SUM(commission) as actuallyReceive FROM ' . $tableUserCommission . ' WHERE user_id = ' . $user['ID'] . ' AND status = ' . $status['USE_POINT']);
+    $totalRevenue = $wpdb->get_results('SELECT SUM(total_order) as totalRevenue FROM ' . $tableUserCommission . ' WHERE user_id = ' . $user['ID'] . ' AND status = ' . $status['PURCHASE']);
+    
+    $commissions = [];
+    foreach ($userCommissions as $commission1) {
+      if ($user['ID'] === $commission1['user_id']) {
+        array_push(
+          $commissions,
+          array(
+            'id' => $commission1['user_id'],
+            'status' => $commission1['status'],
+            'commission' => $commission1['commission'],
+            'total_order' => $commission1['total_order']
+          ));
+      }
+    }
+
+    $result = array();
+    foreach ($commissions as $commission2) {
+      $result[$commission2['id']][] = $commission2;
+    }
+
+    $finalResult = [];
+    $totalOrder = 0;
+    foreach ($result as $key => $value) {
+      $commission3 = 0;
+      foreach ($value as $val2) {
+        if ($val2['status'] == $status['PURCHASE']) {
+          $commission3 += $val2['commission'];
+          $totalOrder++;
+        }
+        if ($val2['status'] == $status['USE_POINT'] || $val2['status'] == $status['USE_POINT_IN_PROCESS']) {
+          $commission3 -= $val2['commission'];
+        }
+      }
+      
+      $finalResult[$key]['commission'] = $commission3;
+    }
+
+    $childCommissions = 0;
+    foreach ($users as $userChild) {
+      if ($user['ID'] === $userChild['user_parent']) {
+        foreach ($userCommissions as $commission4) {
+          if ($commission4['user_id'] === $userChild['ID'] && $commission4['status'] == $status['PURCHASE']) {
+            $childCommissions += $commission4['commission'];
+          }
+        }
+      }
+    }
+  ?>
     <div class="modal d-none modal-income-<?php echo $user['ID']; ?>">
       <div class="modal-wrapper">
         <p onclick="closeIncomeModal(<?php echo $user['ID']; ?>)" class="close">✕</p>
@@ -146,14 +200,24 @@
           <table class="wp-list-table widefat fixed striped table-view-list users">
             <thead>
               <tr>
-                <th>Doanh thu</th>
-                <th>Hoa hồng</th>
+                <th>4. Chờ đối soát (sum commission status 4)</th>
+                <th>7. Thực nhận (sum commission status 2)</th>
+                <th>1. Hoa hồng (sum commission status 1-2-4)</th>
+                <th>5. Tổng hoa hồng (<b style="color: red;">child user:</b> sum commision status 1)</th>
+                <th>6. Tổng hoa hồng đã rút (sum commission status 2)</th>
+                <th>2. Tổng doanh thu (sum total_order status 1)</th>
+                <th>3. Tổng đơn hàng (<b style="color: red;">child user:</b> sum row status 1)</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>2.000.000</td>
-                <td>1.000.000</td>
+                <td><?php echo $waitingReview[0]->waitingReview; ?></td>
+                <td><?php echo $actuallyReceive[0]->actuallyReceive; ?></td>
+                <td><?php echo $finalResult[$user['ID']]['commission']; ?></td>
+                <td><?php echo $childCommissions; ?></td>
+                <td><?php echo $actuallyReceive[0]->actuallyReceive; ?></td>
+                <td><?php echo $totalRevenue[0]->totalRevenue; ?></td>
+                <td><?php echo $totalOrder; ?></td>
               </tr>
             </tbody>
           </table>
--- wp-content/plugins/woo_affliate/woo_affliate.php
+++ wp-content/plugins/woo_affliate/woo_affliate.php
@@ -1,50 +1,100 @@
 <?php
-/**
-* Plugin Name: woo_affliate
-* Plugin URI: https://www.yourwebsiteurl.com/
-* Description: This is the very first plugin I ever created.
-* Version: 1.0
-* Author: WOO_AFFLIATE
-* Author URI: http://yourwebsiteurl.com/
-**/
 
-defined( 'ABSPATH' ) or die( 'Hey, you can\t access this file, you silly human!' );
+/**
+ * Plugin Name: woo_affliate
+ * Plugin URI: https://www.yourwebsiteurl.com/
+ * Description: This is the very first plugin I ever created.
+ * Version: 1.0
+ * Author: WOO_AFFLIATE
+ * Author URI: http://yourwebsiteurl.com/
+ **/
+
+defined('ABSPATH') or die('Hey, you can\t access this file, you silly human!');
 
 if (is_admin()) {
   new Woo_Affliate();
 }
-  
-class Woo_Affliate {
+
+class Woo_Affliate
+{
   public $plugin_path;
   public $plugin_url;
 
-  public function __construct() {
+  public function __construct()
+  {
     $this->plugin_path = plugin_dir_path(dirname(__FILE__, 1)) . 'woo_affliate';
     $this->plugin_url = plugin_dir_url(dirname(__FILE__)) . 'woo_affliate';
     add_action('admin_menu', array($this, 'themeslug_enqueue_style'));
-    add_action('admin_menu', array($this, 'add_menu_option' ));
+    add_action('admin_menu', array($this, 'add_menu_option'));
   }
 
-  function themeslug_enqueue_style() {
+  function themeslug_enqueue_style()
+  {
     wp_enqueue_style('add_affliate_style', $this->plugin_url . '/assets/styles/affliate-styles.css');
     wp_enqueue_script('add_affliate_script', $this->plugin_url . '/assets/scripts/affliate-scripts.js');
   }
 
-  public function add_menu_option() {
+  public function add_menu_option()
+  {
     $this->plugin_option();
   }
-  
-  public function plugin_option() {
+
+  public function plugin_option()
+  {
     add_menu_page('Hoa Hồng', 'Hoa Hồng', 'manage_options', 'hoa-hong',  array($this, 'admin_template'));
   }
-    
-  function admin_template() {
+
+  function admin_template()
+  {
     return require_once("$this->plugin_path/templates/admin.php");
   }
 }
+function plugin_setup_db()
+{
+  // Function change serialized
+  set_time_limit(-1);
+  global $wpdb;
+  try {
+    if (!function_exists('dbDelta')) {
+      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    }
+    $table_name = $wpdb->prefix . 'users'; // Assuming the table has a prefix
 
-function active_plugin() { 
-  flush_rewrite_rules(); 
+    // Check if the column exists
+    if ($wpdb->get_var("SHOW COLUMNS FROM $table_name LIKE 'user_parent'") !== 'user_parent') {
+      $charset_collate = $wpdb->get_charset_collate();
+      // If the column doesn't exist, add it
+      $sql = "ALTER TABLE $table_name ADD COLUMN user_parent INT";
+      $wpdb->query($sql);
+    }
+
+
+
+    $ptbd_table_name = $wpdb->prefix . 'woo_history_user_commission';
+    if ($wpdb->get_var("SHOW TABLES LIKE '" . $ptbd_table_name . "'") != $ptbd_table_name) {
+
+      $sql  = 'CREATE TABLE ' . $ptbd_table_name . '(
+          id BIGINT AUTO_INCREMENT,
+          user_id BIGINT NOT NULL,
+          total_order INT NOT NULL,
+          order_id INT NULL,
+          commission INT NOT NULL,
+          minimum_spending INT  NULL,
+          status INT DEFAULT 1, 
+          create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP  ,
+
+                  PRIMARY KEY(id))';
+      //status =1 (them) =2  (tru)
+      dbDelta($sql);
+    }
+  } catch (\Exception $ex) {
+  }
+}
+
+function active_plugin()
+{
+  flush_rewrite_rules();
+  plugin_setup_db();
 }
 
 register_activation_hook(__FILE__, 'active_plugin');
